cf58584978b39fa6a45ec28bd5cbae5ca0ed7d2c
chipslays <hello@chipslays.ru>
2021-06-16 16:27:50 +0400

    added tests

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -84,4 +84,48 @@ final class ArrTest extends TestCase
 
         $this->assertEquals(print_r(range(1, 10), true), (string) $collection);
     }
+
+    public function testCollectionWhere()
+    {
+        $collection = new Collection([
+            [
+                'user' => 'name1',
+            ],
+            [
+                'user' => 'name2',
+            ],
+            [
+                'user' => 'name3',
+            ],
+        ]);
+
+        $this->assertEquals([1 => ['user' => 'name2']], $collection->where('user', 'name2')->all());
+    }
+
+    public function testCollectionMap()
+    {
+        $result = (new Collection(range(1, 5)))->map(function ($item) {
+            return ++$item;
+        })->all();
+
+        $this->assertEquals(range(2, 6), $result);
+    }
+
+    public function testCollectionMapWithKeys()
+    {
+        $result = (new Collection(range(1, 5)))->mapWithKeys(function ($item) {
+            return [$item => $item];
+        })->all();
+
+        $this->assertEquals([1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5], $result);
+    }
+
+    public function testCollectionFilter()
+    {
+        $result = (new Collection(range(1, 5)))->filter(function ($value, $key) {
+            return $value > 2;
+        })->values()->all();
+
+        $this->assertEquals([3, 4, 5], $result);
+    }
 }
